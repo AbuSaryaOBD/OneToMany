80445a3b1d87e9e097a0b5cb6cd615bf22492fe4
AbuSaryaObd <abukarmo194@gmail.com>
2019-10-14 11:59:19 +0300

    EndSection

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Post;
+use App\User;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +16,21 @@
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/create', function () {
+    $user = User::findOrFail(1);
+    $post = new Post(['title'=>'My first Post','body'=>'I love laravel']);
+    $user->posts()->save($post);
+});
+
+Route::get('/update', function () {
+    $user = User::findOrFail(1);
+    $user->posts()->where('id','=',2)->update(['title'=>'title 2','body'=>'2 That is awesome']);
+});
+
+Route::get('/delete', function () {
+    $user = User::find(1);
+    $user->posts()->whereId(1)->delete();
 });
